cf481daae3930146592a651c86939ad6dcd49c60
antonV <voitekhov_anton@mail.ru>
2021-05-30 18:12:18 +0500

    checking the category of belonging user in NoteController

--- notewebapp/src/main/java/net/ru/voitekhov/notewebapp/controller/NoteController.java
+++ notewebapp/src/main/java/net/ru/voitekhov/notewebapp/controller/NoteController.java
@@ -2,10 +2,10 @@ package net.ru.voitekhov.notewebapp.controller;
 
 
 import net.ru.voitekhov.notewebapp.model.Note;
+import net.ru.voitekhov.notewebapp.service.CategoryService;
 import net.ru.voitekhov.notewebapp.service.NoteService;
+import net.ru.voitekhov.notewebapp.util.SecurityUtil;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -14,41 +14,48 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/note")
 public class NoteController {
 
-    final NoteService service;
+    private final NoteService noteService;
+    private final CategoryService categoryService;
 
     @Autowired
-    public NoteController(NoteService service) {
-        this.service = service;
+    public NoteController(NoteService noteService, CategoryService categoryService) {
+        this.noteService = noteService;
+        this.categoryService = categoryService;
     }
+
 
     @GetMapping("/{categoryId}")
     public String getAllNotes(@PathVariable("categoryId") int categoryId, Model model) {
-        model.addAttribute("notes", service.getAll(categoryId));
+        int userId = SecurityUtil.getAuthUser();
+        if (categoryService.get(categoryId, userId) == null) {
+            return "NotAccess";
+        }
+        model.addAttribute("notes", noteService.getAll(categoryId));
         model.addAttribute("categoryId", categoryId);
         return "allNotes";
     }
 
     @PostMapping("/save/{categoryId}")
     public String save(Note note, @PathVariable("categoryId") int categoryId) {
-        service.save(note, categoryId);
+        noteService.save(note, categoryId);
         return "redirect:/note/" + categoryId;
     }
 
     @GetMapping("/get/{categoryId}/{id}")
     @ResponseBody
     public Note get(@PathVariable("categoryId") int categoryId, @PathVariable("id") int id) {
-        return service.get(id, categoryId);
+        return noteService.get(id, categoryId);
     }
 
     @GetMapping("delete/{categoryId}/{id}")
     public String delete(@PathVariable("categoryId") int categoryId, @PathVariable("id") int id) {
-        service.delete(id, categoryId);
+        noteService.delete(id, categoryId);
         return "redirect:/note/" + categoryId;
     }
 
     @GetMapping("back")
     public String back() {
-        return "redirect:/auth/success";
+        return "redirect:/category";
     }
 
 
